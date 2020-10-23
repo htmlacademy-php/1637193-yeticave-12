4540762ac799a69ce79ccd53364ddc6086ccc738
Aleksandr <xgalkin@gmail.com>
2020-10-23 10:19:24 +0300

    Заменяет цикл while на foreach при работе с одномерным массивом $categories, убирает лишнюю переменную из цикла foreach с двумерным массивом $ad_information
    https://up.htmlacademy.ru/php/12/tasks/5

--- index.php
+++ index.php
@@ -96,15 +96,11 @@ $ad_information = [
             <p class="promo__text">На нашем интернет-аукционе ты найдёшь самое эксклюзивное сноубордическое и горнолыжное снаряжение.</p>
             <ul class="promo__list">
                 <!--заполнил этот список из массива категорий $categories-->
-                <?php
-                $index = 0;
-                $num = count($categories);
-                while ($index < $num): ?>
+                <?php foreach ($categories as $category_key => $category_name): ?>
                     <li class="promo__item promo__item--boards">
-                        <a class="promo__link" href="pages/all-lots.html"><?=$categories[$index];?></a>
+                        <a class="promo__link" href="pages/all-lots.html"><?=$categories[$category_key];?></a>
                     </li>
-                    <?php $index++; ?>
-                <?php endwhile; ?>
+                <?php endforeach; ?>
             </ul>
         </section>
         <section class="lots">
@@ -112,19 +108,19 @@ $ad_information = [
                 <h2>Открытые лоты</h2>
             </div>
             <ul class="lots__list">
-                <!--заполнил этот список из массива $ad_information с товарами-->
-                <?php foreach ($ad_information as $key => $value): ?>
+                <!--заполнил этот список из массива $ad_information с товарами $ad_value-->
+                <?php foreach ($ad_information as $ad_value): ?>
                     <li class="lots__item lot">
                         <div class="lot__image">
-                            <img src="<?=$value['url_image']; ?>" width="350" height="260" alt="<?=$value['title']; ?>">
+                            <img src="<?=$ad_value['url_image']; ?>" width="350" height="260" alt="<?=$ad_value['title']; ?>">
                         </div>
                         <div class="lot__info">
-                            <span class="lot__category"><?=$value['category'];?></span>
-                            <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$value['title'];?></a></h3>
+                            <span class="lot__category"><?=$ad_value['category'];?></span>
+                            <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$ad_value['title'];?></a></h3>
                             <div class="lot__state">
                                 <div class="lot__rate">
                                     <span class="lot__amount">Стартовая цена</span>
-                                    <span class="lot__cost"><?=$value['price'];?><b class="rub">р</b></span>
+                                    <span class="lot__cost"><?=$ad_value['price'];?><b class="rub">р</b></span>
                                 </div>
                                 <div class="lot__timer timer">
                                     12:23
@@ -141,16 +137,12 @@ $ad_information = [
 <footer class="main-footer">
     <nav class="nav">
         <ul class="nav__list container">
-            <!--заполнил этот список из массива категорий $categories-->
-            <?php
-            $index = 0;
-            $num = count($categories);
-            while ($index < $num): ?>
+            <!--заполнил этот список из массива категорий $categories товарами $category_name-->
+            <?php foreach ($categories as $category_key => $category_name): ?>
                 <li class="nav__item">
-                    <a href="pages/all-lots.html"><?=$categories[$index];?></a>
+                    <a href="pages/all-lots.html"><?=$categories[$category_key];?></a>
                 </li>
-                <?php $index++; ?>
-            <?php endwhile; ?>
+            <?php endforeach; ?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
