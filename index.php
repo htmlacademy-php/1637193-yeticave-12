ee6a8cef8273b6c0d152e192c4d82a83541b4536
Aleksandr <xgalkin@gmail.com>
2020-10-27 12:17:29 +0300

    Берет функцию include_template из файла helpers.php
    https://up.htmlacademy.ru/php/12/tasks/8

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-//require_once './helpers.php';
+require_once './helpers.php';
 require_once './functions/data.php';
 
 
@@ -45,29 +45,6 @@ function formatted_sum ($lot_price)
     $round_number = number_format($round_number, 0, ',', ' ')  . ' ' . '₽';
 
     return $round_number;
-}
-
-/**
- * Функция подключает шаблон, передает туда данные и возвращает итоговый HTML контент
- * @param string $name Путь к файлу шаблона относительно папки templates
- * @param array $data Ассоциативный массив с данными для шаблона
- * @return string Итоговый HTML
- */
-function include_template($name, array $data = []) {
-    $name = 'templates/' . $name;
-    $result = '';
-
-    if (!is_readable($name)) {
-        return $result;
-    }
-
-    ob_start();
-    extract($data);
-    require $name;
-
-    $result = ob_get_clean();
-
-    return $result;
 }
 
 $page_content = include_template('main.php', [
