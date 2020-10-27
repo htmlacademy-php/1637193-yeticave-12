227dceae0a900f44e5f655c0edbe4e9d9f791548
Aleksandr <xgalkin@gmail.com>
2020-10-27 12:14:57 +0300

    Добавляет функцию get_date_range для подсчёта оставшегося времени действия лота в аукционе, добавляет в массив $ad_information данные о дате завершения аукциона
    https://up.htmlacademy.ru/php/12/tasks/8

--- index.php
+++ index.php
@@ -14,40 +14,75 @@ $ad_information = [
         'title' => '2014 Rossignol District Snowboard',
         'category' => 'Доски и лыжи',
         'price' => 10999,
-        'url_image' => 'img/lot-1.jpg'
+        'url_image' => 'img/lot-1.jpg',
+        'expiration_date' => '2020-10-28'
     ],
     [
         'title' => 'DC Ply Mens 2016/2017 Snowboard',
         'category' => 'Доски и лыжи',
         'price' => 159999,
-        'url_image' => 'img/lot-2.jpg'
+        'url_image' => 'img/lot-2.jpg',
+        'expiration_date' => '2020-10-29'
     ],
     [
         'title' => 'Крепления Union Contact Pro 2015 года размер L/XL',
         'category' => 'Крепления',
         'price' => 8000,
-        'url_image' => 'img/lot-3.jpg'
+        'url_image' => 'img/lot-3.jpg',
+        'expiration_date' => '2020-10-31'
     ],
     [
         'title' => 'Ботинки для сноуборда DC Mutiny Charocal',
         'category' => 'Ботинки',
         'price' => 10999,
-        'url_image' => 'img/lot-4.jpg'
+        'url_image' => 'img/lot-4.jpg',
+        'expiration_date' => '2020-11-15'
     ],
     [
         'title' => 'Куртка для сноуборда DC Mutiny Charocal',
         'category' => 'Одежда',
         'price' => 7500,
-        'url_image' => 'img/lot-5.jpg'
+        'url_image' => 'img/lot-5.jpg',
+        'expiration_date' => '2020-11-28'
     ],
     [
         'title' => 'Маска Oakley Canopy',
         'category' => 'Разное',
         'price' => 5400,
-        'url_image' => 'img/lot-6.jpg'
+        'url_image' => 'img/lot-6.jpg',
+        'expiration_date' => '2021-06-28'
     ]
 ];
 
+
+/**
+ * Возвращает оставшееся до переданной в функцию даты время в виде массива [ЧЧ, ММ]
+ * @param string $get_end_date Конечная дата в формате 'ГГГГ-ММ-ДД'
+ * @return array Возвращает массив строк в формате [ЧЧ, ММ].
+ */
+//функция для подсчёта оставшегося времени действия лота в аукционе
+function get_date_range($get_end_date)
+{
+        date_default_timezone_set("Europe/Moscow");
+        setlocale(LC_ALL, 'ru_RU');
+        //сегодняшняя дата
+        $current_date = time();
+        //дата окончания аукциона
+		$future_date = strtotime($get_end_date);
+
+		// получает экземпляр временного промежутка на основе разницы между двумя датами
+		$diff = $future_date - $current_date;
+
+		// Приводит временной интервал к нужному формату
+        $hours = floor($diff / 3600);
+        $minutes = ceil(($diff % 3600) / 60);
+
+        //дополняет строки, начиная слева, заданными символами до 2-х цифр
+        $hours_format = str_pad($hours, 2, 0, STR_PAD_LEFT);
+        $minutes_format = str_pad($minutes, 2, 0, STR_PAD_LEFT);
+
+    return [$hours_format, $minutes_format];
+}
 //функция по выводу форматированной суммы товара
 function formatted_sum ($lot_price)
 {
