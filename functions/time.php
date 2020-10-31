7ca6b5a9c67c98930041045f67066a2dd947f6d9
Aleksandr <xgalkin@gmail.com>
2020-10-31 18:04:45 +0300

    Рефакторинг функции get_date_range: дополнительная проверка входящих данных на на соответствие формату 'ГГГГ-ММ-ДД' благодаря функции is_date_valid
    https://up.htmlacademy.ru/php/12/tasks/8

--- functions/time.php
+++ functions/time.php
@@ -7,25 +7,29 @@
 //функция для подсчёта оставшегося времени действия лота в аукционе
 function get_date_range($get_end_date)
 {
-    date_default_timezone_set("Europe/Moscow");
-    setlocale(LC_ALL, 'ru_RU');
-    //сегодняшняя дата
-    $current_date = time();
-    //дата окончания аукциона
-    $future_date = strtotime($get_end_date);
+    if (is_date_valid($get_end_date) == true) {
+        date_default_timezone_set("Europe/Moscow");
+        setlocale(LC_ALL, 'ru_RU');
+        //сегодняшняя дата
+        $current_date = time();
+        //дата окончания аукциона
+        $future_date = strtotime($get_end_date);
 
-    // получает экземпляр временного промежутка на основе разницы между двумя датами
-    $diff = $future_date - $current_date;
-    if ($diff<0) {
+        // получает экземпляр временного промежутка на основе разницы между двумя датами
+        $diff = $future_date - $current_date;
+        if ($diff < 0) {
+            return [0, 0];
+        }
+        // Приводит временной интервал к нужному формату
+        $hours = floor($diff / 3600);
+        $minutes = ceil(($diff % 3600) / 60);
+
+        //дополняет строки, начиная слева, заданными символами до 2-х цифр
+        $hours_format = str_pad($hours, 2, 0, STR_PAD_LEFT);
+        $minutes_format = str_pad($minutes, 2, 0, STR_PAD_LEFT);
+
+        return [$hours_format, $minutes_format];
+    } else {
         return [0, 0];
     }
-    // Приводит временной интервал к нужному формату
-    $hours = floor($diff / 3600);
-    $minutes = ceil(($diff % 3600) / 60);
-
-    //дополняет строки, начиная слева, заданными символами до 2-х цифр
-    $hours_format = str_pad($hours, 2, 0, STR_PAD_LEFT);
-    $minutes_format = str_pad($minutes, 2, 0, STR_PAD_LEFT);
-
-    return [$hours_format, $minutes_format];
 }
