f4c7c9561c5f2bce7fa033e53b51a52eb6fe7a79
Aleksandr <xgalkin@gmail.com>
2020-11-03 13:49:33 +0300

    Рефакторинг базы данных schema.sql: смена названий переменных, добавление описания к столбцам таблиц
    https://up.htmlacademy.ru/php/12/tasks/9

--- schema.sql
+++ schema.sql
@@ -9,78 +9,81 @@ USE yeticave;
 -- Создание таблицы категорий
 CREATE TABLE category
 (
-    category_id    INT          NOT NULL AUTO_INCREMENT PRIMARY KEY, -- первичный ключ, автоматически увеличивается на 1 для новой записи
-    category_title VARCHAR(100) NOT NULL,
-    symbolic_code  VARCHAR(100) NOT NULL,
-    INDEX (category_title)                                           -- создаю индекс для поля, по которому будет поиск
+    id            INT          NOT NULL AUTO_INCREMENT PRIMARY KEY, -- первичный ключ, автоматически увеличивается на 1 для новой записи
+    title         VARCHAR(100) NOT NULL,                            -- Заголовок категорий лотов
+    symbolic_code VARCHAR(100) NOT NULL,                            --  Символьный код категории для назначения правильного класса в меню категорий
+    INDEX category_title (title)                                    -- создаю индекс для поля, по которому будет поиск
 );
 
 -- Создание таблицы с пользователями
 CREATE TABLE users
 (
-    user_id           INT          NOT NULL AUTO_INCREMENT PRIMARY KEY, -- первичный ключ, автоматически увеличивается на 1 для новой записи
-    registration_date DATETIME     NOT NULL,
-    email             VARCHAR(100) NOT NULL UNIQUE,
-    name              VARCHAR(45)  NOT NULL,
-    password          VARCHAR(45)  NOT NULL,
-    contacts          VARCHAR(255) NOT NULL,
-    INDEX users_user_id (user_id)                                       -- создаю индекс для поля, по которому будет поиск
+    id         INT          NOT NULL AUTO_INCREMENT PRIMARY KEY, -- первичный ключ, автоматически увеличивается на 1 для новой записи
+    created_at TIMESTAMP DEFAULT NOW(),                          -- дата регистрации: дата и время, когда этот пользователь завёл аккаунт
+    updated_at TIMESTAMP ON UPDATE NOW(),                        -- дата и время обновления таблицы
+    email      VARCHAR(100) NOT NULL UNIQUE,                     -- email пользователя
+    name       VARCHAR(45)  NOT NULL,                            -- имя пользователя
+    password   VARCHAR(45)  NOT NULL,                            -- пароль: хэшированный пароль пользователя
+    contacts   VARCHAR(255) NOT NULL,                            -- контакты: контактная информация для связи с пользователем
+    INDEX users_id (id)                                          -- создаю индекс для поля, по которому будет поиск
 );
 
 -- Создание таблицы с лотами аукциона
 CREATE TABLE item
 (
-    item_id         INT          NOT NULL AUTO_INCREMENT PRIMARY KEY, -- первичный ключ, автоматически увеличивается на 1 для новой записи
-    creation_date   DATETIME,
-    item_title      VARCHAR(100) NOT NULL,
-    description     VARCHAR(255) NOT NULL,
-    image_url       VARCHAR(255) NOT NULL,
-    start_price     INT          NOT NULL,
-    completion_date DATETIME     NOT NULL,
-    bet_step        INT          NOT NULL,
-    author_id       INT          NOT NULL,
-    category_id     INT          NOT NULL,
-    winner_id       INT          NOT NULL,
-    INDEX item_item_title (item_title),                               -- создаю индекс для поля, по которому будет поиск
-    INDEX item_category_id (category_id),                             -- создаю индекс для поля, по которому будет поиск
-    INDEX item_author_id (author_id),                                 -- создаю индекс для поля, по которому будет поиск
-    CONSTRAINT item_category_category_id                              -- ограничение внешнего ключа
-        FOREIGN KEY (category_id)                                     -- указываю внешний ключ для поля
-            REFERENCES category (category_id)
-            ON DELETE CASCADE                                         -- автоматическое удаление записи по ссылке после удаления в первоисточнике
-            ON UPDATE CASCADE,                                        -- автоматическое обновление записи по ссылке после обновления в первоисточнике
-    CONSTRAINT item_users_author_id                                   -- ограничение внешнего ключа
-        FOREIGN KEY (author_id)                                       -- указываю внешний ключ для поля
-            REFERENCES users (user_id)
-            ON DELETE CASCADE                                         -- автоматическое удаление записи по ссылке после удаления в первоисточнике
-            ON UPDATE CASCADE,                                        -- автоматическое обновление записи по ссылке после обновления в первоисточнике
-    CONSTRAINT item_users_winner_id                                   -- ограничение внешнего ключа
-        FOREIGN KEY (winner_id)                                       -- указываю внешний ключ для поля
-            REFERENCES users (user_id)
-            ON DELETE CASCADE                                         -- автоматическое удаление записи по ссылке после удаления в первоисточнике
-            ON UPDATE CASCADE                                         -- автоматическое обновление записи по ссылке после обновления в первоисточнике
+    id           INT          NOT NULL AUTO_INCREMENT PRIMARY KEY, -- первичный ключ, автоматически увеличивается на 1 для новой записи
+    created_at   TIMESTAMP DEFAULT NOW(),                          -- дата создания: дата и время, когда этот лот был создан пользователем
+    updated_at   TIMESTAMP ON UPDATE NOW(),                        -- дата и время обновления таблицы
+    title        VARCHAR(100) NOT NULL,                            -- название: задается пользователем
+    description  VARCHAR(255) NOT NULL,                            -- описание: задается пользователем
+    image_url    VARCHAR(255) NOT NULL,                            -- изображение: ссылка на файл изображения
+    start_price  INT          NOT NULL,                            -- начальная цена лота
+    completed_at TIMESTAMP,                                        -- дата завершения аукциона по данному лоту
+    bet_step     INT          NOT NULL,                            -- шаг ставки
+    author_id    INT          NOT NULL,                            -- связь: - автор: пользователь, создавший лот;
+    category_id  INT          NOT NULL,                            -- связь: - победитель: пользователь, выигравший лот
+    winner_id    INT          NOT NULL,                            -- связь: - категория: категория объявления
+    INDEX item_title (title),                                      -- создаю индекс для поля, по которому будет поиск
+    INDEX item_category_id (category_id),                          -- создаю индекс для поля, по которому будет поиск
+    INDEX item_author_id (author_id),                              -- создаю индекс для поля, по которому будет поиск
+    CONSTRAINT item_category_idx                                   -- ограничение внешнего ключа
+        FOREIGN KEY (category_id)                                  -- указываю внешний ключ для поля
+            REFERENCES category (id)
+            ON DELETE CASCADE                                      -- автоматическое удаление записи по ссылке после удаления в первоисточнике
+            ON UPDATE CASCADE,                                     -- автоматическое обновление записи по ссылке после обновления в первоисточнике
+    CONSTRAINT item_author_idx                                     -- ограничение внешнего ключа
+        FOREIGN KEY (author_id)                                    -- указываю внешний ключ для поля
+            REFERENCES users (id)
+            ON DELETE CASCADE                                      -- автоматическое удаление записи по ссылке после удаления в первоисточнике
+            ON UPDATE CASCADE,                                     -- автоматическое обновление записи по ссылке после обновления в первоисточнике
+    CONSTRAINT item_winner_idx                                     -- ограничение внешнего ключа
+        FOREIGN KEY (winner_id)                                    -- указываю внешний ключ для поля
+            REFERENCES users (id)
+            ON DELETE CASCADE                                      -- автоматическое удаление записи по ссылке после удаления в первоисточнике
+            ON UPDATE CASCADE                                      -- автоматическое обновление записи по ссылке после обновления в первоисточнике
 );
 
 -- Создание таблицы со ставками
 CREATE TABLE bet
 (
-    bet_id   INT            NOT NULL AUTO_INCREMENT PRIMARY KEY, -- первичный ключ, автоматически увеличивается на 1 для новой записи
-    bet_date DATETIME       NOT NULL,
-    total    DECIMAL(10, 2) NOT NULL,
-    user_id  INT            NOT NULL,
-    item_id  INT            NOT NULL,
-    INDEX bet_user_id (user_id),                                 -- создаю индекс для поля, по которому будет поиск
-    INDEX bet_item_id (item_id),                                 -- создаю индекс для поля, по которому будет поиск
-    CONSTRAINT bet_item_item_id                                  -- ограничение внешнего ключа
-        FOREIGN KEY (item_id)                                    -- указываю внешний ключ для поля
-            REFERENCES item (item_id)
-            ON DELETE CASCADE                                    -- автоматическое удаление записи по ссылке после удаления в первоисточнике
-            ON UPDATE CASCADE,                                   -- автоматическое обновление записи по ссылке после обновления в первоисточнике
-    CONSTRAINT bet_users_user_id                                 -- ограничение внешнего ключа
-        FOREIGN KEY (user_id)                                    -- указываю внешний ключ для поля
-            REFERENCES users (user_id)
-            ON DELETE CASCADE                                    -- автоматическое удаление записи по ссылке после удаления в первоисточнике
-            ON UPDATE CASCADE                                    -- автоматическое обновление записи по ссылке после обновления в первоисточнике
+    id         INT            NOT NULL AUTO_INCREMENT PRIMARY KEY, -- первичный ключ, автоматически увеличивается на 1 для новой записи
+    created_at TIMESTAMP DEFAULT NOW(),                            -- дата и время размещения ставки
+    updated_at TIMESTAMP ON UPDATE NOW(),                          -- дата и время обновления таблицы
+    total      DECIMAL(10, 2) NOT NULL,                            -- сумма: цена, по которой пользователь готов приобрести лот
+    user_id    INT            NOT NULL,                            -- связь: - пользователь, сделавший ставку
+    item_id    INT            NOT NULL,                            -- связь: - лот, на который сделали ставку
+    INDEX bet_user_id (user_id),                                   -- создаю индекс для поля, по которому будет поиск
+    INDEX bet_item_id (item_id),                                   -- создаю индекс для поля, по которому будет поиск
+    CONSTRAINT bet_item_idx                                        -- ограничение внешнего ключа
+        FOREIGN KEY (item_id)                                      -- указываю внешний ключ для поля
+            REFERENCES item (id)
+            ON DELETE CASCADE                                      -- автоматическое удаление записи по ссылке после удаления в первоисточнике
+            ON UPDATE CASCADE,                                     -- автоматическое обновление записи по ссылке после обновления в первоисточнике
+    CONSTRAINT bet_user_idx                                        -- ограничение внешнего ключа
+        FOREIGN KEY (user_id)                                      -- указываю внешний ключ для поля
+            REFERENCES users (id)
+            ON DELETE CASCADE                                      -- автоматическое удаление записи по ссылке после удаления в первоисточнике
+            ON UPDATE CASCADE                                      -- автоматическое обновление записи по ссылке после обновления в первоисточнике
 );
 
 
