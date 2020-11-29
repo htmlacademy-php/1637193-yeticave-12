2d374d0520ec8a8f5022c692cd74d478b2e07977
Aleksandr <xgalkin@gmail.com>
2020-11-29 19:23:38 +0300

    Рефакторинг: корректировка ссылок
    https://up.htmlacademy.ru/php/12/tasks/14

--- templates/layout.php
+++ templates/layout.php
@@ -23,28 +23,28 @@
     <header class="main-header">
         <div class="main-header__container container">
             <h1 class="visually-hidden">YetiCave</h1>
-            <a class="main-header__logo" href="/index.php">
+            <a class="main-header__logo" href="../index.php">
                 <img src="../img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
             </a>
             <form class="main-header__search" method="get" action="https://echo.htmlacademy.ru" autocomplete="off">
                 <input type="search" name="search" placeholder="Поиск лота">
                 <input class="main-header__search-btn" type="submit" name="find" value="Найти">
             </form>
-            <a class="main-header__add-lot button" href="/add.php">Добавить лот</a>
+            <a class="main-header__add-lot button" href="../add.php">Добавить лот</a>
             <nav class="user-menu">
                 <?php if ($is_auth == 1): ?>
                     <div class="user-menu__logged">
                         <p><?= $user_name ?></p>
-                        <a class="user-menu__bets" href="pages/my-bets.html">Мои ставки</a>
+                        <a class="user-menu__bets" href="../pages/my-bets.html">Мои ставки</a>
                         <a class="user-menu__logout" href="#">Выход</a>
                     </div>
                 <?php else: ?>
                     <ul class="user-menu__list">
                         <li class="user-menu__item">
-                            <a href="/sign-up.php">Регистрация</a>
+                            <a href="../sign-up.php">Регистрация</a>
                         </li>
                         <li class="user-menu__item">
-                            <a href="/enter.php">Вход</a>
+                            <a href="../enter.php">Вход</a>
                         </li>
                     </ul>
                 <?php endif; ?>
@@ -120,7 +120,7 @@
                 </svg>
             </a>
         </div>
-        <a class="main-footer__add-lot button" href="/add.php">Добавить лот</a>
+        <a class="main-footer__add-lot button" href="../add.php">Добавить лот</a>
         <div class="main-footer__developed-by">
             <span class="visually-hidden">Разработано:</span>
             <a class="logo-academy" href="https://htmlacademy.ru/intensive/php">
@@ -135,7 +135,7 @@
     </div>
 </footer>
 
-<script src="flatpickr.js"></script>
-<script src="script.js"></script>
+<script src="../flatpickr.js"></script>
+<script src="../script.js"></script>
 </body>
 </html>
