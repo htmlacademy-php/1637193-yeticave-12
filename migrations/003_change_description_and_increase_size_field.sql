07b412371d03cbda7c6a34f95d220716873f89ac
Aleksandr <xgalkin@gmail.com>
2020-12-03 18:11:39 +0300

    Изменяет порядок корректировки БД, добавляет тестового пользователя для проверки авторизации
    https://up.htmlacademy.ru/php/12/tasks/15

--- migrations/003_change_description_and_increase_size_field.sql
+++ migrations/003_change_description_and_increase_size_field.sql
@@ -1,6 +1,12 @@
 -- Используем базу данных
 USE yeticave;
 
+-- Изменение типа поля description в таблице item на TEXT, т.к. VARCHAR(255) мал по размеру
+ALTER TABLE item
+    CHANGE description description TEXT
+        CHARACTER SET utf8
+            COLLATE utf8_general_ci NOT NULL;
+
 -- Добавление большего по размеру описания для лотов после изменения типа поля
 UPDATE item
 SET item.description = 'Lorem ipsum dolor, sit amet, consectetur adipisicing elit. Veritatis alias quam aperiam, earum sunt, nulla id quis cum totam eaque deleniti qui eligendi nisi beatae? Nisi reprehenderit debitis error voluptate.
@@ -8,15 +14,12 @@ Nulla doloribus repudiandae velit dolore distinctio assumenda nihil, sequi ipsa 
 Sunt nihil in, similique vero laborum id exercitationem. Ratione corrupti eum voluptate itaque, dolorem temporibus doloremque voluptatum incidunt odio animi officiis ipsa ullam dignissimos. Accusantium omnis, eius reiciendis nesciunt vero!'
 WHERE item.id BETWEEN 1 AND 6;
 
--- Изменение типа поля description в таблице item на TEXT, т.к. VARCHAR(255) мал по размеру
-ALTER TABLE item
-    CHANGE description description TEXT
-        CHARACTER SET utf8
-            COLLATE utf8_general_ci NOT NULL;
-
 -- Увеличение количества символов в поле "Пароль" в таблице users для сохранения ХЕШ-пароля в БД
 ALTER TABLE users
     CHANGE password
         password VARCHAR(60)
             CHARACTER SET utf8
                 COLLATE utf8_general_ci NULL DEFAULT NULL;
+
+-- Добавление 1го тестового аккаунта для проверки авторизации
+INSERT INTO users (email, name, password, contacts) VALUES ('test@test.ru', 'test account', '90b1f057fbcaf3d1a42dd905ab4e35de', 'пароль от аккаунта: password123 . Хеш получен с сайта http://md5-online.ru/')
