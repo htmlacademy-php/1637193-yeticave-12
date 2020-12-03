951f3ca0f3c3f5c845cfe787700cdee95f546a9c
Aleksandr <xgalkin@gmail.com>
2020-12-03 17:52:57 +0300

    Добавляет страницу входа зарегистрированного пользователя enter.php, шаблон формы входа enter_page.php, корректирует функцию проверки указанного e-mail в БД
    https://up.htmlacademy.ru/php/12/tasks/15

--- functions/check.php
+++ functions/check.php
@@ -3,7 +3,7 @@
 /**
  * Функция проверяет, заполнено ли указанное поле
  * @param $name string Проверяемое поле в форме
- * @param $name_in_russian string|null Название поля на русском языке либо NULL
+ * @param $name_in_russian string Название поля на русском языке
  * @return string|null В случае незаполненности возвращает требование о необходимости добавить данные либо NULL
  */
 function validate_filled(string $name, $name_in_russian)
@@ -135,15 +135,26 @@ function validate_email(string $email)
 }
 
 /**
+ * Функция запроса на поиск в БД записи в таблице пользователей по введенному в форме email
+ * @param mysqli $connect Данные о подключении к БД
+ * @return mysqli_result Переданный запрос в БД
+ */
+function check_unique_email(mysqli $connect)
+{
+    $check_email = mysqli_real_escape_string($connect, $_POST['email']); //экранирование спец.символов для использования в SQL-выражении
+    $check_sql = "SELECT * FROM users WHERE email = '$check_email'"; // запрос на поиск записи в таблице пользователей по переданному email
+
+    return mysqli_query($connect, $check_sql); // передаем запрос в БД;
+}
+
+/**
  * Функция валидации уникальности адреса электронной почты, в случае успешной валидации возвращает NULL
  * @param mysqli $connect Данные о подключении к БД
  * @return string|null Сообщение о том, что пользователь под данным e-mail уже зарегистрирован или NULL при отсутствии ошибок
  */
 function validate_unique_email(mysqli $connect)
 {
-    $check_email = mysqli_real_escape_string($connect, $_POST['email']); //экранирование спец.символов для использования в SQL-выражении
-    $check_sql = "SELECT id FROM users WHERE email = '$check_email'"; // запрос на поиск записи в таблице пользователей по переданному email
-    $check_result = mysqli_query($connect, $check_sql); // передаем запрос в БД
+    $check_result = check_unique_email($connect);
 
     if (mysqli_num_rows($check_result) > 0) {
         return 'Пользователь с этим email уже зарегистрирован';
@@ -163,6 +174,36 @@ function validate_contacts(string $contacts)
     }
     if (strlen($contacts) > 255) {
         return "Контакты должны занимать менее 255 символов";
+    }
+    return NULL;
+}
+
+
+/**
+ * Функция проверки наличия зарегистрированного пользователя в БД
+ * @param mysqli_result $check_result_sql Переданный запрос в БД о поиске пользователя с email, введенным в форме $form
+ * @param array $errors Массив с ошибками валидации формы
+ * @param array $form Массив с введенными значениями в форме
+ * @return array|string|null Массив с записью о новом пользователе в случае совпадения хеша пароля и введенного пароля пользователем либо строку с описанием ошибки валидации, либо NULL
+ */
+function check_user_db (mysqli_result $check_result_sql, array $errors, array $form): ?string
+{
+// Если в результате проверки пользователь есть в результате запроса из БД,
+// тогда эти данные о нем получаем в виде нового ассоциативного массива,
+// иначе данных о пользователе в БД еще нет
+    $user = $check_result_sql ? mysqli_fetch_array($check_result_sql, MYSQLI_ASSOC) : null;
+
+    if (!count($errors) and $user) {
+        //Проверяем, что сохраненный хеш пароля и введенный пароль из формы совпадают.
+        if (password_verify($form['password'], $user['password'])) {
+            //если совпадение есть, значит пользователь указал верный пароль.
+            // Тогда мы можем открыть для него сессию и записать в неё все данные о пользователе
+            return $_SESSION['user'] = $user;
+        } //иначе пароль неверный и мы добавляем сообщение об этом в список ошибок
+            return $errors['password'] = 'Неверный пароль: проверьте введенные символы на корректность';
+
+    } elseif (empty($errors['email'])) { //Если пользователь не найден, то записываем это как ошибку валидации
+        return $errors['email'] = 'Пользователь с указанным e-mail не зарегистрирован на сайте: проверьте правильность набора email';
     }
     return NULL;
 }
