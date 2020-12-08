d9bd99702f1422f1d2cc5a7bedefb56a63c21d5c
Aleksandr <xgalkin@gmail.com>
2020-12-08 14:59:09 +0300

    Рефакторинг пагинации на странице поиска
    https://up.htmlacademy.ru/php/12/tasks/16

--- templates/search_page.php
+++ templates/search_page.php
@@ -3,7 +3,6 @@
  * @var array $ad_information
  * @var array $pages
  * @var string $search
- * @var string $back_page
  * @var int $current_page
  * @var int $pages_count
  *
@@ -32,7 +31,8 @@
                                     <span
                                         class="lot__cost"><?= htmlspecialchars(formatted_sum($ad_value['total'])) ?></span>
                                 </div>
-                                <div class="lot__timer timer<?php if ($remaining_time[0] == '00'): ?>timer--finishing<?php endif; ?>">
+                                <div
+                                    class="lot__timer timer<?php if ($remaining_time[0] == '00'): ?>timer--finishing<?php endif; ?>">
                                     <?php if ($remaining_time[0] == '00' && $remaining_time[1] == '00'): ?>
                                         Время лота истекло
                                     <?php else: ?>
@@ -47,37 +47,25 @@
                 <p>Ничего не найдено по вашему запросу</p>
             <?php endif; ?>
         </ul>
-        <!--        <ul class="lots__list">-->
-        <!--            <li class="lots__item lot">-->
-        <!--                <div class="lot__image">-->
-        <!--                    <img src="../img/lot-1.jpg" width="350" height="260" alt="Сноуборд">-->
-        <!--                </div>-->
-        <!--                <div class="lot__info">-->
-        <!--                    <span class="lot__category">Доски и лыжи</span>-->
-        <!--                    <h3 class="lot__title"><a class="text-link" href="lot.html">2014 Rossignol District Snowboard</a>-->
-        <!--                    </h3>-->
-        <!--                    <div class="lot__state">-->
-        <!--                        <div class="lot__rate">-->
-        <!--                            <span class="lot__amount">Стартовая цена</span>-->
-        <!--                            <span class="lot__cost">10 999<b class="rub">р</b></span>-->
-        <!--                        </div>-->
-        <!--                        <div class="lot__timer timer">-->
-        <!--                            16:54:12-->
-        <!--                        </div>-->
-        <!--                    </div>-->
-        <!--                </div>-->
-        <!--            </li>-->
-        <!--        </ul>-->
     </section>
-    <?php if ($pages_count > 1): ?>
+    <?php if ($pages_count > 0): ?>
         <ul class="pagination-list">
-                <li class="pagination-item pagination-item-prev"><a a href="/search.php?search=<?=$search;?>&find=Найти&page=<?php if($current_page > 1) {echo $back_page = $current_page - 1;} else {echo $back_page = 1;} ?>>Назад</a></li>
+            <li class="pagination-item pagination-item-prev">
+                <a a href="/search.php?search=<?= $search; ?>&find=Найти&page=<?php if ($current_page > 1) {
+                                                                    echo $back_page = $current_page - 1;
+                                                                } else {
+                                                                    echo $back_page = 1;
+                                                                } ?>>Назад</a></li>
                 <?php foreach ($pages as $page): ?>
-                    <li class=" pagination-item <?php if ($page == $current_page): ?> pagination-item-active<?php endif; ?>">
-                      <a href="/search.php?search=<?=$search;?>&find=Найти&page=<?=$page;?>"><?=$page;?></a>
-                    </li>
-                <?php endforeach; ?>
-                <li class="pagination-item pagination-item-next"><a href="#">Вперед</a></li>
+                    <li class=" pagination-item <?php if ($page === $current_page): ?>pagination-item-active<?php endif; ?>">
+                <a href="/search.php?search=<?= $search; ?>&find=Найти&page=<?= $page; ?>"><?= $page; ?></a>
+            </li>
+            <?php endforeach; ?>
+            <li class="pagination-item pagination-item-next"><a href="/search.php?search=<?= $search; ?>&find=Найти&page=<?php if ($current_page < $pages_count) {
+                        echo $current_page += 1;
+                    } else {
+                        echo $current_page = $pages_count;
+                    } ?>">Вперед</a></li>
         </ul>
     <?php endif; ?>
 </div>
