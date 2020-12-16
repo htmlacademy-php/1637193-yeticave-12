5b5bd73f6f4d15fd48de2515d3f7a09d21752ac5
Aleksandr <xgalkin@gmail.com>
2020-12-16 20:18:44 +0300

    Рефаторинг пагинации: убрал ошибки до рабочего состояния

--- templates/pagination.php
+++ templates/pagination.php
@@ -14,13 +14,13 @@
         <li class="pagination-item pagination-item-prev">
             <?php if ($current_page > 1): ?>
                 <?php $back_page = ($current_page > 1) ? ($current_page - 1) : "1" ?>
-                <a href="/<?= $this_page; ?>?<?php if (isset($search) && $search !== ''): ?>&search=<?= $search; ?><?php endif; ?><?php if (isset($category_id) && $category_id !== ''): ?>&id=<?= $category_id; ?><?php endif; ?><?= htmlspecialchars($search); ?>&page=<?= $back_page; ?>">Назад</a>
+                <a href="/<?= $this_page; ?>?<?php if (isset($search) && $search !== ''): ?>&search=<?= htmlspecialchars($search); ?><?php endif; ?><?php if (isset($category_id) && $category_id !== ''): ?>&id=<?= $category_id; ?><?php endif; ?>&page=<?= $back_page; ?>">Назад</a>
             <?php endif; ?>
         </li>
         <?php foreach ($pages as $page): ?>
             <?php if ($page !== '...'): ?>
                 <li class="pagination-item <?php if ($page === $current_page): ?>pagination-item-active<?php endif; ?>">
-                    <a href="/<?= $this_page; ?>?<?php if (isset($search) && $search !== ''): ?>&search=<?= $search; ?><?php endif; ?><?php if (isset($category_id) && $category_id !== ''): ?>&id=<?= $category_id; ?><?php endif; ?><?= htmlspecialchars($search); ?>&page=<?= $page; ?>"><?= $page; ?></a>
+                    <a href="/<?= $this_page; ?>?<?php if (isset($search) && $search !== ''): ?>&search=<?= htmlspecialchars($search); ?><?php endif; ?><?php if (isset($category_id) && $category_id !== ''): ?>&id=<?= $category_id; ?><?php endif; ?>&page=<?= $page; ?>"><?= $page; ?></a>
                 </li>
             <?php else: ?>
                 <li class="pagination-item">&#8230;</li>
@@ -29,7 +29,7 @@
         <li class="pagination-item pagination-item-next">
             <?php if ($current_page < $pages_count): ?>
                 <?php $current_page = ($current_page < $pages_count) ? ($current_page + 1) : $pages_count ?><a
-                href="/<?= $this_page; ?>?<?php if (isset($search) && $search !== ''): ?>&search=<?= $search; ?><?php endif; ?><?php if (isset($category_id) && $category_id !== ''): ?>&id=<?= $category_id; ?><?php endif; ?><?= htmlspecialchars($search); ?>&page=<?= $current_page ?>">
+                href="/<?= $this_page; ?>?<?php if (isset($search) && $search !== ''): ?>&search=<?= htmlspecialchars($search); ?><?php endif; ?><?php if (isset($category_id) && $category_id !== ''): ?>&id=<?= $category_id; ?><?php endif; ?>&page=<?= $current_page ?>">
                     Вперед</a>
             <?php endif; ?></li>
     </ul>
