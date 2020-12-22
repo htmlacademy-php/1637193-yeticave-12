5c5b4a3bac6da60e1dfa60ef4c289fd5f185954c
Aleksandr <xgalkin@gmail.com>
2020-12-22 11:01:11 +0300

    Рефакторинг отправки сообщений победителям лотов: вынос в отдельные функции запросов в БД о списке ставок, определении победителей и отправке писем
    https://up.htmlacademy.ru/php/12/tasks/18

--- getwinner.php
+++ getwinner.php
@@ -4,89 +4,25 @@ require_once 'vendor/autoload.php'; //подключает файл автоза
 
 $connect = db_connection();
 
-$user_name = '';
-$item_title = '';
-$item_id = '';
-
-// указываем данные для доступа к SMTP-серверу
-$transport = new Swift_SmtpTransport("phpdemo.ru", 25);
-$transport->setUsername("keks@phpdemo.ru");
-$transport->setPassword("htmlacademy");
-
-//Создадим главный объект библиотеки SwiftMailer, ответственный за отправку сообщений.
-// Передадим туда созданный объект с SMTP-сервером.
-$mailer = new Swift_Mailer($transport);
-
-//Сформируем запрос, который покажет список последних ставок по лотам без победителей,
-// дата истечения которых меньше или равна текущей
-$sql_bet_winner = 'SELECT bet.id AS bet_id, total, bet.user_id AS user_id, item_id, bet.created_at AS created_at, i.winner_id AS winner_id,
-               i.title AS item_title, i.completed_at AS completed_at, u.name AS user_name, u.email AS user_email
-        FROM bet
-        INNER JOIN item i on bet.item_id = i.id
-        INNER JOIN users u on bet.user_id = u.id
-        INNER JOIN (
-                SELECT item_id AS item_id_bet, MAX(total) AS max_bet
-                FROM bet
-                GROUP BY item_id
-        ) bet_new ON bet.item_id = bet_new.item_id_bet AND bet.total = bet_new.max_bet
-        WHERE i.winner_id IS NULL AND i.completed_at <= CURDATE()
-        ORDER BY bet_id';
-
-
-$result_bet_winner = mysqli_query($connect, $sql_bet_winner);
-
-if (!$result_bet_winner) {
-    exit('Ошибка запроса: &#129298; ' . mysqli_error($connect));
-}
+// список последних ставок по лотам без победителей, дата истечения которых меньше или равна текущей
+$result_finished_lots = get_finished_lots($connect);
 
 //Получим список победитилей в виде массива
-if ($result_bet_winner && mysqli_num_rows($result_bet_winner)) {
-    $winners = mysqli_fetch_all($result_bet_winner, MYSQLI_ASSOC);
+if ($result_finished_lots && mysqli_num_rows($result_finished_lots)) {
+    $winners = mysqli_fetch_all($result_finished_lots, MYSQLI_ASSOC);
 
-    if (!empty($winners)) {
-        foreach ($winners as $winner) {
-            //Установим параметры сообщения: тема, отправитель и список его получателей
-            $message = new Swift_Message();
-            $message->setSubject("Ваша ставка победила");
-            $message->setFrom(['keks@phpdemo.ru' => 'keks@phpdemo.ru']);
-            $message->setTo([$winner['user_email'] => $winner['user_name']]);
+    //определяем победителей конкретных лотов
+    foreach ($winners as $winner) {
+        if ($winner['winner_id'] === null) {
 
-            //данные для вставки в шаблон:
-            $user_name = $winner['user_name'];
-            $item_title = $winner['item_title'];
-            $item_id = (int)$winner['item_id'];
-            $host_url = $_SERVER['SERVER_NAME'];
+            $user = (int)$winner['user_id'];
+            $item = (int)$winner['item_id'];
 
-            //Передадим список победителей в шаблон, используемый для отправки email
-            $message_content = include_template(
-                '/email.php',
-                [
-                    'user_name' => $user_name,
-                    'item_title' => $item_title,
-                    'item_id' => $item_id,
-                    'host_url' => $host_url
-                ]
-            );
-            $message->setBody($message_content, 'text/html');
-
-            //Передадим в объект библиотеки SwiftMailer, ответственный за отправку сообщений, созданный объект с SMTP-сервером
-            $mailer = new Swift_Mailer($transport);
-
-            if ($winner['winner_id'] === null) {
-                //Отправляем подготовленное сообщение и получаем результат
-                $result = $mailer->send($message);
-
-                $user = (int)$winner['user_id'];
-                $item = (int)$winner['item_id'];
-
-                $sql_winner = "UPDATE item SET winner_id = $user WHERE id = $item";
-
-                $result_winner = mysqli_query($connect, $sql_winner);
-
-                if (!$result_winner) {
-                    exit('Ошибка запроса: &#129298; ' . mysqli_error($connect));
-                }
-            }
+            identify_winner_lot($user, $item);
         }
+    }
+    if (!empty($winners)) {
+        //отправка письма победителю лота
+        array_map("send_email_to_winner", $winners);
     }
 }
--- templates/email.php
+++ templates/email.php
@@ -3,12 +3,12 @@
  * @var string $user_name
  * @var string $item_title
  * @var string $host_url
- * @var string $item_id
-*/
+ * @var string $item_link
+ */
 ?>
 <h1>Поздравляем с победой</h1>
 <p>Здравствуйте, <?= htmlspecialchars($user_name); ?></p>
-<p>Ваша ставка для лота <a href="<?= $host_url . "/lot.php?id=" . $item_id ?>"><?= htmlspecialchars($item_title) ?></a>
+<p>Ваша ставка для лота <a href="<?= $host_url . $item_link ?>"><?= htmlspecialchars($item_title) ?></a>
     победила.</p>
 <p>Перейдите по ссылке <a href="<?= $host_url ?>/my_bets.php">мои ставки</a>,
     чтобы связаться с автором объявления</p>
